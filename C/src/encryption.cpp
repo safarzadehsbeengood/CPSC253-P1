216b7f1d79e9731a2493c5bb4cdc3c49ae016cee
Ryan Safa <safarzadehryan@gmail.com>
2024-09-21 13:01:38 -0700

    Started CPP version

---
+++ C/src/encryption.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <iomanip>
+#include "file_io.cpp"
+
+int main() {
+    std::cout << "Hello";
+    return 0;
+}
---
+++ C/src/encryption.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+std::string encrypt(std::string pt, std::string key) {
+    return "test";
+}
+
+std::string decrypt(std::string ct, std::string key) {
+    return "test";
+}
