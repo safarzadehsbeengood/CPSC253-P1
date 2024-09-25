fc36b5a532907cdd33633120e49a4bee2785c64e
Ryan Safa <safarzadehryan@gmail.com>
2024-09-24 17:06:35 -0700

    cleaned up CLI interface

--- Python/main.py
+++ Python/main.py
@@ -1,25 +1,31 @@
 import sys
 from src.file_io import decrypt_file, encrypt_file
+from src.keygen import keygen
         
 if __name__ == "__main__":
     choice = None
-    print("Welcome! Would you like to:\n\t1. Encrypt\n\t2. Decrypt\n\tq: Cancel\n")
+    print("Welcome! Would you like to:\n\t1. Encrypt\n\t2. Decrypt\n\tq: Cancel")
     while choice is None:
         choice = input("> ")
         if choice == '1' or choice == '2' or choice == 'q':
             break
         print("Invalid input, please try again.")
+        choice = None
     
     if choice != 'q':
         input_file = input("Enter an input file: ")
         output_file = input("Enter an output file: ")
-        key = input("Enter a 16-character key (alphanumeric characters only): ")
-        while len(key) != 16 and not key.isalnum():
-            key = input("Invalid format, try again: ")
+        key = input("Enter a 16-character key (alphanumeric characters only) or 'keygen' for a random key: ")
+        while len(key) != 16 and not key.isalnum() and key != 'q' and key != 'keygen':
+            key = input("Invalid format, try again (q to cancel): ")
+        if key == 'keygen':
+            key = keygen()
         if choice == '1':
             encrypt_file(input_file, output_file, key)
+            print(f'{input_file} encrypted to {output_file} with key {key}.')
         else:
             decrypt_file(input_file, output_file, key)
+            print(f'{input_file} decrypted to {output_file} with key {key}.')
     else:
         sys.exit()
             
--- Python/test_file_io.py
+++ Python/test_file_io.py
@@ -3,7 +3,7 @@ from src.keygen import keygen
 
 key = keygen()
 
-encrypt_file("voyage.txt", "voyage_encrypted.txt", key)
-decrypt_file("voyage_encrypted.txt", "voyage_decrypted.txt", key)
+encrypt_file("txt/voyage.txt", "txt/voyage_encrypted.txt", key)
+decrypt_file("txt/voyage_encrypted.txt", "txt/voyage_decrypted.txt", key)
 
 print(f'key: {key}')
